3327820b127ace4530cb68a6b98c2edd09a20e92
Tazik Shahjahan <35576188+taziksh@users.noreply.github.com>
2021-09-16 21:56:24 -0400

    Fix stale avatar URL

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Avatar as Base } from 'theme-ui'
-import TImage from "../images/T.png"
-
 
 const Avatar = ({ size = 48, ...props }) => (
   <Base
     {...props}
     size={size}
-    src={TImage}
+    src="https://cdn.myanimelist.net/r/360x360/images/characters/14/34899.jpg?s=07c1a0ab44efe1a1386e7caa7a8d4764"
     alt="Tazik's avatar"
     mr={3}
   />
